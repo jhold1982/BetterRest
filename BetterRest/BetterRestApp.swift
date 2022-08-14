0259a30e46f9b9cd89645c65c187597e60928ad0
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2022-08-14 11:07:26 -0500

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Justin Hold on 8/14/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
